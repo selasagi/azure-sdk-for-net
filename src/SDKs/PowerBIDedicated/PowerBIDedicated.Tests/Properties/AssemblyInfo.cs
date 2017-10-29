998000422143ec5318f94ac5f7e5a865371a050c
Sagi Sela <sagis@microsoft.com>
2017-10-29 08:02:36 +0000

    Update PowerBI dedicated project

--- src/SDKs/PowerBIDedicated/PowerBIDedicated.Tests/Properties/AssemblyInfo.cs
+++ src/SDKs/PowerBIDedicated/PowerBIDedicated.Tests/Properties/AssemblyInfo.cs
@@ -2,19 +2,18 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-/*
+
 [assembly: AssemblyTitle("PowerBIDedicated.Tests")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("PowerBIDedicated.Tests Class Library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("MSIT")]
+[assembly: AssemblyCompany("Microsoft Corporation")]
 [assembly: AssemblyProduct("PowerBIDedicated.Tests")]
-[assembly: AssemblyCopyright("Copyright © MSIT 2013")]
+[assembly: AssemblyCopyright("Copyright (c) Microsoft Corporation")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,7 +33,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-*/
-    [assembly: ComVisible(false)]
-    // The following GUID is for the ID of the typelib if this project is exposed to COM
-    [assembly: Guid("bc70f362-fd99-4b13-8807-ff93fe06eff8")]
+
+[assembly: ComVisible(false)]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bc70f362-fd99-4b13-8807-ff93fe06eff8")]
